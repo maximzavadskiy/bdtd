d119234da98f58d3cce492ea78810ddadb8bbcf8
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2019-10-29 14:14:58 +0200

    App bar in welcome screen

--- imports/ui/Welcome.jsx
+++ imports/ui/Welcome.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import routes from '/imports/ui/constants'
+import Header from '/imports/ui/Header'
+
 
 function Copyright() {
     return (
@@ -55,6 +52,8 @@ export default function SignIn() {
     const classes = useStyles();
 
     return (
+        <React.Fragment>
+            <Header title="Home" />
         <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
@@ -89,5 +88,7 @@ export default function SignIn() {
                 <Copyright />
             </Box>
         </Container>
+        </React.Fragment>
+
     );
 }
