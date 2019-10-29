97d346f8e3a9544ff0d90c473bb405b81a1df683
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2019-10-29 11:45:37 +0200

    Add Flow Router with / route

--- client/main.jsx
+++ client/main.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import App from '/imports/ui/App';
-import '/imports/config.js';
+import '/imports/config';
 
-Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { mount } from 'react-mounter';
+
+// FlowRouter automatically mounts React, no need for .render & Meteor.startup
+FlowRouter.route('/', {
+  name: 'Welcome',
+  action() {
+    mount(App)
+  },
 });
+
+
--- imports/config.js
+++ imports/config.js
@@ -1,33 +1,4 @@
 import { Accounts } from 'meteor/accounts-base';
-console.log('Configuring things')
 Accounts.ui.config({
     passwordSignupFields: 'EMAIL_ONLY', // name & problem title will be in the problem form
-    // defaultLayoutType: 'blaze-to-react',
-    // defaultTemplate: 'fullPageAtForm',  // default
-    // defaultLayout: MainLayout,
-    // defaultContentRegion: 'content',
-    // defaultLayoutRegions: {
-        // nav: <Nav />,
-        // footer: <Footer />
-    // },
-
-    // sendVerificationEmail: true,
-    // enforceEmailVerification: true,
-    // confirmPassword: true,
-    // continuousValidation: false,
-    // displayFormLabels: true,
-    // forbidClientAccountCreation: true,
-    // formValidationFeedback: true,
-    // homeRoutePath: '/',
-    // showAddRemoveServices: false,
-    // showPlaceholders: true,
-
-    // negativeValidation: true,
-    // positiveValidation: true,
-    // negativeFeedback: false,
-    // positiveFeedback: true,
-
-    // Privacy Policy and Terms of Use
-    // privacyUrl: 'privacy',
-    // termsUrl: 'terms-of-use',
 });
