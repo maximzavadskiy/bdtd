2cdf1957202b19bd86d7b9e5abc9d64c032c7e4e
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2019-10-30 12:46:31 +0200

    Add placeholder problem list

--- imports/ui/FindProblem.jsx
+++ imports/ui/FindProblem.jsx
@@ -1,9 +1,129 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+import Header from './Header';
+import { withTracker } from 'meteor/react-meteor-data';
+import _ from 'lodash';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import ImageIcon from '@material-ui/icons/Image';
+import WorkIcon from '@material-ui/icons/Work';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
 
-const FindProblem = () => (
-    <div>
-        <h1> TODO </h1>
-    </div>
-);
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                BeenThereDoneThat
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
-export default FindProblem;
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        position: 'relative',
+    },
+    layout: {
+        width: 'auto',
+        marginLeft: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+            width: 600,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+        padding: theme.spacing(2),
+        [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+            marginTop: theme.spacing(6),
+            marginBottom: theme.spacing(6),
+            padding: theme.spacing(3),
+        },
+    },
+    stepper: {
+        padding: theme.spacing(3, 0, 5),
+    },
+    buttons: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+    },
+    button: {
+        marginTop: theme.spacing(3),
+        marginLeft: theme.spacing(1),
+    },
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+
+
+function FindProblem({ user }) {
+    const classes = useStyles();
+
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <Header title="Find a Mentee" />
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    {_.isEmpty(user) && <Typography variant="h6">
+                        You need to Sign In to access this page
+                            </Typography>}
+                    {!_.isEmpty(user) && <React.Fragment> 
+                        <Typography variant="h6">
+                            Mentor Requests
+                        </Typography>
+                        <List className={classes.root}>
+                            <ListItem button>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <ImageIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="Photos" secondary="11 Oct by Maxim Zavadskiy, Software Engineer " />
+
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <WorkIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="Work" secondary="Jan 7, 2014" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <BeachAccessIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary="Vacation" secondary="July 20, 2014" />
+                            </ListItem>
+                    </List> </React.Fragment>}
+                </Paper>
+                <Copyright />
+            </main>
+        </React.Fragment>
+    );
+}
+
+export default FindProblemContainer = withTracker(() => {
+    return {
+        user: Meteor.user(),
+    };
+})(FindProblem);
